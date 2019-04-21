2658ad0f123a208ad718ea0e3ba514263b7ea1f8
artem-galas <artem.galas@gmail.com>
2019-04-21 19:49:38 +0300

    [Chapter 2. First Test]
    - Create LogAn classlib project
    - Create LogAn.UnitTests project And add reference
    - Implement Simple tests

---
+++ Chap02_FirstTest/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,83 @@
+using System;
+
+using NUnit.Framework;
+
+using LogAn;
+
+namespace Tests
+{
+    public class LogAnalyzerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            // Arrange
+            LogAnalyzer logAnalyzer = new LogAnalyzer();
+
+            // Act
+            bool result = logAnalyzer.IsValidLogFileName("file_with_bad_extention.foo");
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtentionLowerCase_ReturnsTrue()
+        {
+            LogAnalyzer logAnalyzer = new LogAnalyzer();
+            bool result = logAnalyzer.IsValidLogFileName("file_with_good_extention.slf");
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_GoodExtentionUpperCase_ReturnsTrue()
+        {
+            LogAnalyzer logAnalyzer = new LogAnalyzer();
+            bool result = logAnalyzer.IsValidLogFileName("file_with_good_extention.SLF");
+
+            Assert.True(result);
+        }
+
+        // parameterized test - refactor previous two tests
+        [TestCase("file_with_good_extention.SLF")]
+        [TestCase("file_with_good_extention.slf")]
+        public void IsValidLogFileName_ValidExtention_ReturnTrue(string fileName)
+        {
+            LogAnalyzer logAnalyzer = new LogAnalyzer();
+
+            bool result = logAnalyzer.IsValidLogFileName(fileName);
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_Throws()
+        {
+            LogAnalyzer logAnalyzer = new LogAnalyzer();
+
+            var exception = Assert.Catch<Exception>(() => logAnalyzer.IsValidLogFileName(""));
+
+            StringAssert.Contains("filename has to be provided", exception.Message);
+        }
+
+        // fluent syntax in assertion
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_ThrowsArgumentException()
+        {
+            LogAnalyzer logAnalyzer = new LogAnalyzer();
+
+            var exception = Assert.Throws<ArgumentException>(() => logAnalyzer.IsValidLogFileName(""));
+
+            Assert.That(
+                exception.Message, 
+                Does.Contain("filename has to be provided")
+            );
+        }
+    }
+}
---
+++ Chap02_FirstTest/LogAn/LogAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) {
+                throw new ArgumentException("filename has to be provided");
+            }
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
